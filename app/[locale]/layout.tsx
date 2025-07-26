030d7f07f7582c1daa51c1de06a3057ed0cd312b
Issam Kharbache <issamkharbache2@gmail.com>
2025-07-26 20:05:39 +0100

    changing the layout font

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,13 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import "../globals.css";
+import { Quicksand } from "next/font/google";
+
+const quickSand = Quicksand({
+    subsets: ["latin"],
+    display: "swap",
+});
+
 export default async function LocaleLayout({
     children,
     params,
@@ -14,5 +21,9 @@ export default async function LocaleLayout({
     if (!hasLocale(routing.locales, locale)) {
         notFound();
     }
-    return <NextIntlClientProvider>{children}</NextIntlClientProvider>;
+    return (
+        <NextIntlClientProvider>
+            <div className={`${quickSand.className}`}>{children}</div>
+        </NextIntlClientProvider>
+    );
 }

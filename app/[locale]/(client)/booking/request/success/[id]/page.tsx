c20d912ba1856bdfae21059f8395803ac61fc05c
Issam kharbache <issamkharbache2@gmail.com>
2025-09-09 12:58:38 +0000

    fix(page): correct casing of component name and variable; fix typo in bookingId

--- app/[locale]/(client)/booking/request/success/[id]/page.tsx
+++ app/[locale]/(client)/booking/request/success/[id]/page.tsx
@@ -1,10 +1,10 @@
 import SuccessBookingRequest from "@/components/activities/booking/SuccessBookingRequest";
 import React from "react";
 
-const page = async ({ params }: { params: { id: string } }) => {
-  const boookingId = (await params).id;
+const Page = async ({ params }: { params: { id: string } }) => {
+  const bookingId = params.id; // just access directly
 
-  return <SuccessBookingRequest bookingId={boookingId} />;
+  return <SuccessBookingRequest bookingId={bookingId} />;
 };
 
-export default page;
+export default Page;

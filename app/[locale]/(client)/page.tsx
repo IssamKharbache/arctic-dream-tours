734ceb56dcbd25defd5bd08377a9c9a4d24055b9
Issam kharbache <issamkharbache2@gmail.com>
2025-12-05 16:59:27 +0000

    fix(HomePage): remove commented-out blog data fetching and BlogCarouselSection

--- app/[locale]/(client)/page.tsx
+++ app/[locale]/(client)/page.tsx
@@ -1,18 +1,10 @@
 import { ActivitiesSection } from "@/components/activities/ActivitySection";
-import { BlogCarouselSection } from "@/components/blog/BlogCarouselSection";
 import HeroSection from "@/components/hero/HeroSection";
 import PromoDialog from "@/components/hero/PromoDialog";
 import Gallery from "@/components/main/Gallery";
 import { WhyChooseUs } from "@/components/main/WhyChooseUs";
-import { getData } from "@/lib/getData";
-import { baseUrl } from "@/utils/baseUrl";
-import { Blog } from "@prisma/client";
 
 const HomePage = async () => {
-  // const response = await getData<{ data: Blog[] }>(
-  //   `${baseUrl}/api/blog/get-all`
-  // );
-  // const blogs = response.data;
   return (
     <div>
       <PromoDialog />
@@ -26,11 +18,8 @@ const HomePage = async () => {
       />
       {/* why choose us*/}
       <WhyChooseUs />
-      {/* <PremiumServices /> */}
       {/* Gallery */}
       <Gallery />
-      {/* Blog Carousel Section */}
-      {/* {blogs.length > 0 ? <BlogCarouselSection blogs={blogs} /> : null} */}
     </div>
   );
 };
